f51c6009ea03a303f39ed7edc3ffb0c3d98628c8
Michael Bauer <mbauer.mphil@googlemail.com>
2023-05-24 21:41:37 +0200

    correctly handle rest-parameters in `Curried` type

--- src/Currying.ts
+++ src/Currying.ts
@@ -1,16 +1,18 @@
 import {type SpreadableElement} from './definitions';
 
-export type Curried<F extends ((..._: any) => any)> =
-    F extends ((a: any) => any) ? F :
-    	F extends ((a: infer A, ...b: infer B) => infer O) ?
-    		B extends never ?
-    			(a: A) => O :
-    			(a: A) => Curried<(...b: B) => O> :
-    		F extends ((a: infer A, b: infer B) => infer O) ?
-    			B extends never ?
-    				(a: A) => O :
-    				(a: A) => Curried<(b: B) => O> :
-    			never;
+type Curried<F extends ((..._: any) => any)> =
+  F extends ((a: any) => any)
+  	? F
+  	: F extends ((a: infer A, ...b: infer B) => infer O)
+  		? B extends never
+  			? (parameter: A) => O
+  			: B extends [c: infer C, ...d: infer D]
+  				// eslint-disable-next-line @typescript-eslint/ban-types
+  				? D extends []
+  					? (parameter: A) => (parameter: C) => O
+  					: Curried<(a: A) => (c: C) => Curried<(...d: D) => O>>
+  				: Curried<(a: A) => (...b: B) => O>
+  		: never;
 
 function curry1<A, O>(f: (a: A, ..._: any[]) => O): ((a: A) => (...rest: SpreadableElement[]) => O) {
 	return (a: A) => (...rest: SpreadableElement[]) => f(a, ...rest);
@@ -24,5 +26,5 @@ export function curry<F extends ((..._: any) => any)>(f: F): Curried<F> {
 		hasMoreArgs = currFn.length > 1;
 	}
 
-	return currFn as unknown as Curried<F>;
+	return currFn as any as Curried<F>;
 }
